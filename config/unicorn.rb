b53f1313ac07bce7247e3c563d0c37dacbf4197c
anbuKakashi <kakafuad12@gmail.com>
2018-05-14 16:21:06 +0700

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,7 @@
 # set path to the application
 app_dir  = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
+shared_dir = File.expand_path('../../../shared/', __FILE__)
+#shared_dir = "#{app_dir}/shared"
 working_directory app_dir
 
 # Set unicorn options
@@ -18,3 +19,24 @@ stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # Set proccess id path
 pid "#{shared_dir}/pids/unicorn.pid"
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
+
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{app_dir}/Gemfile"
+end
